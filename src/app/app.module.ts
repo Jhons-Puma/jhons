6b16e0b267344c9f8c0c6930043f7141cab263fc
Jhons Puma <jhonpuma100@gmail.com>
2024-06-22 08:49:06 -0500

    version1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { ListarClientesComponent } from './listar-clientes/listar-clientes.component';
+import { RegistrarClientesComponent } from './registrar-clientes/registrar-clientes.component';
+import { ActualizarClientesComponent } from './actualizar-clientes/actualizar-clientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaPrincipalComponent,
+    ListarClientesComponent,
+    RegistrarClientesComponent,
+    ActualizarClientesComponent
   ],
   imports: [
     BrowserModule,
